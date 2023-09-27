8f2c4f417f8c731e6219c6fad0f2a10fe04cb8b9
Kaludia27 <claudia.c.a2712@gmail.com>
2023-09-27 10:19:27 +0200

    Trabajo inicial script mov

--- TestInicio/Assets/Script/Movement.cs
+++ TestInicio/Assets/Script/Movement.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb= gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+  
+    private void FixedUpdate() // Se llama si o si 100 veces por sec
     {
         
     }
